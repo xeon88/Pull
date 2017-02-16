a936053fd2c196d6048c4f242d1dd1adc0a5d51e
Marco Corona <Marco Corona>
2017-02-17 00:25:11 +0100

    add filter units

--- common/js/utility.js
+++ common/js/utility.js
@@ -29,10 +29,11 @@
     }
 
 
-    function getIdListByRarity(rarity, characters) {
+    function getIdListByRarity(rarity, characters, farmable) {
         var ids = [] ;
         for (var i=0 ; i<characters.length ; i++){
 
+            /*
             if(i>=104 && i<114) {continue;}  // turtles;
             if(i>=346 && i<351) {continue;}  // Porcs;
             if(i>=299 && i<304) {continue;}  // lobsters;
@@ -46,10 +47,17 @@
             if(i==574 || i==1452 ) {continue;}  // not exists
             if(i>=193 && i<197 ) {continue;} // navy hq
             if(i>=241 && i<246) {continue;} // baroque
+            */
 
-
-            if(characters[i][3]==rarity){
-                ids.push(i+1);
+            var index = farmable.indexOf(i+1);
+            if(index!==-1){
+               console.log( "id : " + i +  "  farmable index : " + index);
+            }
+            else{
+                if(characters[i][3]==rarity){
+                    console.log("add : " + (i+1));
+                    ids.push(i+1);
+                }
             }
         }
         return ids;
@@ -73,3 +81,71 @@
     }
 
 
+
+    function getFarmableList() {
+
+        var idsFarmable = [];
+        for (var i= 0; i<window.drops['Story Island'].length ; i++){
+            var island = window.drops['Story Island'][i];
+            idsFarmable = idsFarmable.concat( island.thumb);
+            var chapters = island.chapters;
+            for(var chapter in chapters){
+                idsFarmable = idsFarmable.concat(chapters[chapter]);
+            }
+            idsFarmable = uniq(idsFarmable);
+        }
+
+
+
+        for(var i = 0; i<window.drops['Weekly Island'].length ; i++){
+            var extraIsland = window.drops['Weekly Island'][i];
+            idsFarmable = idsFarmable.concat( extraIsland.drops);
+        }
+         idsFarmable = uniq(idsFarmable);
+
+
+        for(var i = 0; i<window.drops['Fortnight'].length ; i++){
+            var fortnight = window.drops['Fortnight'][i];
+            console.log(fortnight);
+            idsFarmable = idsFarmable.concat( fortnight.Expert);
+            idsFarmable = idsFarmable.concat( fortnight.Elite);
+            idsFarmable = idsFarmable.concat( fortnight['All Difficulties']);
+            idsFarmable = idsFarmable.concat( fortnight['Japan']);
+            idsFarmable = idsFarmable.concat( fortnight['Global']);
+            idsFarmable = idsFarmable.concat( fortnight.thumb);
+        }
+
+        idsFarmable = uniq(idsFarmable);
+
+        for(var i = 0; i<window.drops['Raid'].length ; i++){
+            var raid = window.drops['Raid'][i];
+            idsFarmable = idsFarmable.concat( raid.Ultimate);
+            idsFarmable = idsFarmable.concat( raid.Master);
+            idsFarmable = idsFarmable.concat( raid.thumb);
+
+        }
+
+
+        for(var i = 0; i<window.drops['Special'].length ; i++){
+            var special = window.drops['Special'][i];
+            console.log(special);
+            idsFarmable = idsFarmable.concat( special['All Difficulties']);
+            idsFarmable = idsFarmable.concat( special['Japan']);
+            idsFarmable = idsFarmable.concat( special['Global']);
+            idsFarmable = idsFarmable.concat( special.thumb);
+            idsFarmable = idsFarmable.concat( special.Chaos);
+
+        }
+
+        idsFarmable = uniq(idsFarmable);
+
+        // console.log(idsFarmable);
+        return idsFarmable;
+    }
+
+
+    function uniq(a) {
+        return a.sort().filter(function(item, pos, ary) {
+        return !pos || item != ary[pos - 1];
+        })
+    }
